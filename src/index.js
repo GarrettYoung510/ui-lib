c05c01c68a7d60b6df05555aeca4d193b1b62ad1
Garrett Young <GarrettYoung510@Gmail.com>
2019-09-29 14:35:02 -0700

    tooltip

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+// // import the css file
+// import "./test.css";
+// console.log("test");
 
+import Tooltip from "./ui/tooltip";
+
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector(".tooltip"));
+
+tooltip.init();
